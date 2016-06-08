79783c9fd53476eaf06f9454cdc6a7f7e4974ea5
Jerry Samuels Jr <jsamuelsjr@gmail.com>
2016-06-08 18:12:04 +0000

    Submitting the only unique elements Ruby assignment.

--- lib/unique_elements.rb
+++ lib/unique_elements.rb
@@ -2,5 +2,15 @@
 # combination of the above), and returns an array of the elements that appear once and only
 # once.
 def find_unique_elements(arr)
-  
+
+arr = [1, 2, "three", "three", 4]
+counts = Hash.new 0
+arr.each do |ele|
+	counts[ele] += 1
+end
+
+counts.delete_if {|key, value| value > 1 }
+result = counts.keys #Array returned with keys for non-dupe values
+puts result
+
 end
