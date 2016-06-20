5f03b536863e39d53a6c8212f4eb69c4929ba1f6
Jerry Samuels Jr <jsamuelsjr@gmail.com>
2016-06-20 19:17:57 +0000

    Corrected last line in function defintion by replacing puts with return.

--- lib/unique_elements.rb
+++ lib/unique_elements.rb
@@ -2,7 +2,6 @@
 # combination of the above), and returns an array of the elements that appear once and only
 # once.
 def find_unique_elements(arr)
-
 	counts = Hash.new 0
 	arr.each do |ele|
 		counts[ele] += 1
@@ -10,5 +9,5 @@ def find_unique_elements(arr)
 	
 	counts.delete_if {|key, value| value > 1 }
 	result = counts.keys #Array returned with keys for non-dupe values
-	puts result
+	return result
 end
